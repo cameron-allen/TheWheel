046e34409fe8f96ddb8b65952d0935eafd734e3c
Cameron Allen <cameronallen86@yahoo.com>
2025-11-21 19:12:11 -0800

    added fstream to pch.h

--- include/pch.h
+++ include/pch.h
@@ -8,3 +8,22 @@ constexpr bool DISPLAY_VULKAN_INFO = true;
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <fstream>
+
+static std::vector<char> ReadFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("<ReadFile> failed to open file!");
+    }
+
+    std::vector<char> buffer(file.tellg());
+
+    file.seekg(0, std::ios::beg);
+    file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+
+    file.close();
+    
+    return buffer;
+}
+
