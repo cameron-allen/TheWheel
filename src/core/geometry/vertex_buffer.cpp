773922b6c4a3b44500fd0b86beb4364add49b42c
Cameron Allen <cameronallen86@yahoo.com>
2026-02-20 20:40:11 -0800

    adjusted vertex_buffer initBuffer to use Buffer::Create for the staging buffer

--- src/core/geometry/vertex_buffer.cpp
+++ src/core/geometry/vertex_buffer.cpp
@@ -1,29 +1,24 @@
 #include "core/geometry/buffers.h"
-#include "core/engine.h"
+
 
 void VertexBuffer::initBuffer(vk::raii::Device& device)
 {
     vk::DeviceSize bufferSize = sizeof(mp_vertices[0]) * mp_vertices->size();
 
-    vk::BufferCreateInfo stagingInfo{ .size = bufferSize, .usage = vk::BufferUsageFlagBits::eTransferSrc, .sharingMode = vk::SharingMode::eExclusive };
-    vk::raii::Buffer stagingBuffer(device, stagingInfo);
-    vk::MemoryRequirements memRequirementsStaging = stagingBuffer.getMemoryRequirements();
-    vk::MemoryAllocateInfo memoryAllocateInfoStaging{ .allocationSize = memRequirementsStaging.size, 
-        .memoryTypeIndex = Buffer::FindMemoryType(memRequirementsStaging.memoryTypeBits, vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) };
-    vk::raii::DeviceMemory stagingBufferMemory(device, memoryAllocateInfoStaging);
-
-    stagingBuffer.bindMemory(stagingBufferMemory, 0);
-    void* dataStaging = stagingBufferMemory.mapMemory(0, stagingInfo.size);
-    memcpy(dataStaging, mp_vertices->data(), stagingInfo.size);
+    vk::raii::Buffer stagingBuffer({});
+    vk::raii::DeviceMemory stagingBufferMemory({});
+    Buffer::Create(
+        device,
+        bufferSize,
+        vk::BufferUsageFlagBits::eTransferSrc,
+        vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent,
+        stagingBuffer,
+        stagingBufferMemory);
+        
+    void* data = stagingBufferMemory.mapMemory(0, bufferSize);
+    memcpy(data, mp_vertices->data(), (size_t)bufferSize);
     stagingBufferMemory.unmapMemory();
-
-    vk::BufferCreateInfo bufferInfo{ .size = bufferSize, .usage = vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eTransferDst, .sharingMode = vk::SharingMode::eExclusive };
-    m_buffer = vk::raii::Buffer(device, bufferInfo);
-
-    vk::MemoryRequirements memRequirements = m_buffer.getMemoryRequirements();
-    vk::MemoryAllocateInfo memoryAllocateInfo{ .allocationSize = memRequirements.size, .memoryTypeIndex = Buffer::FindMemoryType(memRequirements.memoryTypeBits, vk::MemoryPropertyFlagBits::eDeviceLocal) };
-    m_bufferMemory = vk::raii::DeviceMemory(device, memoryAllocateInfo);
-
-    m_buffer.bindMemory(*m_bufferMemory, 0);
-    Buffer::Copy(device, stagingBuffer, m_buffer, stagingInfo.size);
+        
+    Buffer::Create(device, bufferSize, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer, vk::MemoryPropertyFlagBits::eDeviceLocal, m_buffer, m_bufferMemory);
+    Buffer::Copy(device, stagingBuffer, m_buffer, bufferSize);
 }
